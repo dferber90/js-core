200dcb3568094c9e742348845728c216e57c513e
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-04-27 10:37:12 -0700

    feat: Process flags and segments

--- packages/sdk/vercel/src/vercelFeatureStore.ts
+++ packages/sdk/vercel/src/vercelFeatureStore.ts
@@ -8,19 +8,17 @@ import type {
   LDFeatureStoreKindData,
   interfaces,
 } from '@launchdarkly/js-server-sdk-common-edge';
-import { noop } from '@launchdarkly/js-server-sdk-common-edge';
-// import { SerializedItemDescriptor } from '@launchdarkly/js-server-sdk-common/dist/api/interfaces';
+
+import { processFlag, processSegment, noop } from '@launchdarkly/js-server-sdk-common-edge';
 
 interface VercelStorage {
   [namespace: string]: {
-    [key: string]: {
-      version: number;
-      deleted?: boolean;
-    };
+    // TODO: Consider export of flag and segment type.
+    [key: string]: any;
   };
 }
 
-class VercelFeatureStore implements interfaces.PersistentDataStore {
+class VercelFeatureStore implements LDFeatureStore {
   private edgeConfig: EdgeConfigClient;
 
   private configKey: string;
@@ -33,133 +31,79 @@ class VercelFeatureStore implements interfaces.PersistentDataStore {
     this.logger = logger;
   }
 
-  init(
-    allData: interfaces.KindKeyedStore<interfaces.PersistentStoreDataKind>,
-    callback: () => void
-  ): void {
-    callback();
+  async get(
+    kind: DataKind,
+    flagKey: string,
+    callback: (res: LDFeatureStoreItem | null) => void
+  ): Promise<void> {
+    this.logger.debug(`Requesting ${flagKey} from ${this.configKey}`);
+    try {
+      // TODO: Does this return a copy or the same thing in memory? If it isn't a copy
+      // then we may process flags that have already been processed. This may be ok.
+      const config = await this.edgeConfig.get(this.configKey);
+      if (config === null) {
+        this.logger.error('Feature data not found in Edge Config.');
+        callback(null);
+      }
+      const kindKey = kind.namespace === 'features' ? 'flags' : kind.namespace;
+      const item = config as VercelStorage;
+      if (kind.namespace === 'features') {
+        callback(processFlag(item[kindKey][flagKey]));
+      } else if (kind.namespace === 'segments') {
+        callback(processSegment(item[kindKey][flagKey]));
+      }
+    } catch (err) {
+      this.logger.error(err);
+      callback(null);
+    }
   }
 
-  async get(
-    kind: interfaces.PersistentStoreDataKind,
-    key: string,
-    callback: (descriptor: interfaces.SerializedItemDescriptor | undefined) => void
-  ): Promise<void> {
-    this.logger.debug(`Requesting ${key} from ${this.configKey}`);
+  async all(kind: DataKind, callback: (res: LDFeatureStoreKindData) => void = noop): Promise<void> {
+    const kindKey = kind.namespace === 'features' ? 'flags' : kind.namespace;
+    this.logger.debug(`Requesting all ${kindKey} data from Edge Config.`);
     try {
       const config = await this.edgeConfig.get(this.configKey);
       if (config === null) {
         this.logger.error('Feature data not found in Edge Config.');
-        callback(undefined);
-        return;
       }
-      const kindKey = kind.namespace === 'features' ? 'flags' : kind.namespace;
-      const storage = config as VercelStorage;
-      const serializedItem = JSON.stringify(storage[kindKey][key]);
-
-      const descriptor: interfaces.SerializedItemDescriptor = {
-        version: storage[kindKey][key].version,
-        deleted: storage[kindKey][key].deleted,
-        serializedItem,
-      };
-      // const item = config as LDFeatureStoreItem;
-      callback(descriptor);
+      const items = config as VercelStorage;
+      callback(
+        items[kindKey].forEach((item: any) => {
+          if (kind.namespace === 'features') {
+            processFlag(item);
+          }
+          if (kind.namespace === 'segments') {
+            processSegment(item);
+          }
+        })
+      );
     } catch (err) {
       this.logger.error(err);
-      callback(undefined);
+      callback({});
     }
   }
 
-  getAll(
-    kind: interfaces.PersistentStoreDataKind,
-    callback: (
-      descriptors: KeyedItem<string, interfaces.SerializedItemDescriptor>[] | undefined
-    ) => void
-  ): void {
-    throw new Error('Method not implemented.');
+  async initialized(callback: (isInitialized: boolean) => void = noop): Promise<void> {
+    const config = await this.edgeConfig.get(this.configKey);
+    const result = config !== null;
+    this.logger.debug(`Is ${this.configKey} initialized? ${result}`);
+    callback(result);
   }
 
-  upsert(
-    kind: interfaces.PersistentStoreDataKind,
-    key: string,
-    descriptor: interfaces.SerializedItemDescriptor,
-    callback: (
-      err?: Error | undefined,
-      updatedDescriptor?: interfaces.SerializedItemDescriptor | undefined
-    ) => void
-  ): void {
-    throw new Error('Method not implemented.');
-  }
-
-  initialized(callback: (isInitialized: boolean) => void): void {
-    throw new Error('Method not implemented.');
-  }
-
-  close(): void {
-    throw new Error('Method not implemented.');
+  init(allData: LDFeatureStoreDataStorage, callback: () => void): void {
+    callback();
   }
 
   getDescription(): string {
-    throw new Error('Method not implemented.');
+    return 'Vercel Edge Config';
   }
 
-  // async get(
-  //   kind: DataKind,
-  //   flagKey: string,
-  //   callback: (res: LDFeatureStoreItem | null) => void
-  // ): Promise<void> {
-  //   this.logger.debug(`Requesting ${flagKey} from ${this.configKey}`);
-  //   try {
-  //     const config = await this.edgeConfig.get(this.configKey);
-  //     if (config === null) {
-  //       this.logger.error('Feature data not found in Edge Config.');
-  //     }
-  //     const kindKey = kind.namespace === 'features' ? 'flags' : kind.namespace;
-  //     const item = config as LDFeatureStoreItem;
-  //     callback(item[kindKey][flagKey]);
-  //   } catch (err) {
-  //     this.logger.error(err);
-  //     callback(null);
-  //   }
-  // }
+  // unused
+  close = noop;
 
-  // async all(kind: DataKind, callback: (res: LDFeatureStoreKindData) => void = noop): Promise<void> {
-  //   const kindKey = kind.namespace === 'features' ? 'flags' : kind.namespace;
-  //   this.logger.debug(`Requesting all ${kindKey} data from Edge Config.`);
-  //   try {
-  //     const config = await this.edgeConfig.get(this.configKey);
-  //     if (config === null) {
-  //       this.logger.error('Feature data not found in Edge Config.');
-  //     }
-  //     const item = config as LDFeatureStoreItem;
-  //     callback(item[kindKey]);
-  //   } catch (err) {
-  //     this.logger.error(err);
-  //     callback({});
-  //   }
-  // }
+  delete = noop;
 
-  // async initialized(callback: (isInitialized: boolean) => void = noop): Promise<void> {
-  //   const config = await this.edgeConfig.get(this.configKey);
-  //   const result = config !== null;
-  //   this.logger.debug(`Is ${this.configKey} initialized? ${result}`);
-  //   callback(result);
-  // }
-
-  // init(allData: LDFeatureStoreDataStorage, callback: () => void): void {
-  //   callback();
-  // }
-
-  // getDescription(): string {
-  //   return 'Vercel Edge Config';
-  // }
-
-  // // unused
-  // close = noop;
-
-  // delete = noop;
-
-  // upsert = noop;
+  upsert = noop;
 }
 
 export default VercelFeatureStore;
--- packages/shared/sdk-server/src/store/index.ts
+++ packages/shared/sdk-server/src/store/index.ts
@@ -1,4 +1,6 @@
 import AsyncStoreFacade from './AsyncStoreFacade';
 
+import { processFlag, processSegment } from './serialization';
+
 // eslint-disable-next-line import/prefer-default-export
-export { AsyncStoreFacade };
+export { AsyncStoreFacade, processFlag, processSegment };
